d777a1138123178e9dd7b368d4af3374f5eee447
satyaan <satyaandm@gmail.com>
2022-10-12 23:09:12 -0400

    username is unique

--- App/models/user.py
+++ App/models/user.py
@@ -9,7 +9,7 @@ ACCESS = {
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
     access = db.Column(db.Integer, nullable=False)
     reviews = db.relationship(
